ad12ac3dcfd1e2f8c2c6798a76dd50d2210b63f3
gsingh84 <gsingh84@mail.greenriver.edu>
2018-01-19 22:44:42 -0500

    added existing files to repo

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//Gursimran Singh
+//01/19/18
+//index.php
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Set debug level
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new View;
+    echo $view->render
+    ('/pages/home.html');
+});
+
+//Run fat free
+$f3->run();
